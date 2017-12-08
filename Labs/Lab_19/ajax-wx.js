ba1b1a3c90fbf875c6f6a7b9db5771b0fcef9e4e
sainos@uw.edu <sainos>
2017-12-07 18:38:34 -0800

    Finished lab 19

--- Labs/Lab_19/ajax-wx.js
+++ Labs/Lab_19/ajax-wx.js
@@ -7,7 +7,7 @@ $(function() {
       $(this).remove();
     });
     $.ajax({
-      url: "http://api.wunderground.com/api/<yourkey>/geolookup/conditions/q/WA/Bothell.json",
+      url: "http://api.wunderground.com/api/a2fb20dd598019bc/geolookup/conditions/q/WA/Bothell.json",
       dataType: "jsonp",
       success: function(parsed_json) {
         var city = parsed_json['location']['city'];
@@ -16,15 +16,28 @@ $(function() {
         var rh = parsed_json['current_observation']['relative_humidity'];
         var str = "<li> Location : " + city + ", " + state + "</li>";
 
-        // Update list items with data from above...
-        // $('ul').append(str);
-        // $('ul li:last').attr('class', 'list-group-item');
-        // str = "<li> Current temperature is: " + temp_f + "</li>";
-        // $('ul').append(str);
-        // $('ul li:last').attr('class', 'list-group-item');
-        // str = "<li> Relative Humidity is: " + rh + "</li>";
-        // $('ul').append(str);
-        // $('ul li:last').attr('class', 'list-group-item');
+        var wind = parsed_json['current_observation']['wind_string'];
+        var weather = parsed_json['current_observation']['weather'];
+        var icon = parsed_json['current_observation']['icon_url'];
+        var str = "<li style=\"text-align: center\"> <img src=\"" + icon + "\"></li>";
+
+       $('ul').append(str);
+       $('ul li:last').attr('class', 'list-group-item');
+       var str = "<li> Location : " + city + ", " + state + "</li>";
+       $('ul').append(str);
+       $('ul li:last').attr('class', 'list-group-item');
+       var str = "<li> Current temperature is: " + temp_f + "</li>";
+       $('ul').append(str);
+       $('ul li:last').attr('class', 'list-group-item');
+       var str = "<li> Relative Humidity is: " + rh + "</li>";
+       $('ul').append(str);
+       $('ul li:last').attr('class', 'list-group-item');
+       var str = "<li> Weather Today is: " + weather + "</li>";
+       $('ul').append(str);
+       $('ul li:last').attr('class', 'list-group-item');
+       var str = "<li> Wind: " + wind + "</li>";
+       $('ul').append(str);
+       $('ul li:last').attr('class', 'list-group-item');
         console.log("Current temperature in " + city + " is: " + temp_f);
       }
     });
